dba7cd20a098f0a1505c05527f57ef66a7a07d05
joao <joaocosta@zonalivre.org>
2011-02-23 19:25:37 +0000

    autotools specfile based on fedora template

--- specfile/build.sh
+++ specfile/build.sh
@@ -4,3 +4,10 @@ rm -fR ~/myrpms/
 mkdir -p ~/myrpms/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 tar -czPf ~/myrpms/SOURCES/lib_mysqludf_ta-0.1.tar.gz src/*
 rpmbuild -v -bb --clean specfile/lib_mysqludf_ta.spec
+
+
+
+rm -fR ~/rpmbuild
+rpmdev-setuptree
+tar -czPf ~/rpmbuild/SOURCES/lib_mysqludf_ta-0.1.tar.gz *
+rpmbuild -v -bb --clean specfile/lib_mysqludf_ta.spec
