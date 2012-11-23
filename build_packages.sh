904fe6433cb6ab437a0b5e46218cf93b7717731d
João Costa <joaocosta@zonalivre.org>
2012-11-23 22:56:42 +0000

    Add revision number

--- build_packages.sh
+++ build_packages.sh
@@ -3,6 +3,7 @@
 set -e
 
 INSTALLDIR=/tmp/libmysqludf_build_install
+RELEASE=2
 
 rm -fR $INSTALLDIR
 mkdir -p $INSTALLDIR/usr/share/libmysqludf_ta
@@ -12,7 +13,7 @@ make
 make install DESTDIR=$INSTALLDIR
 
 cp src/db_{uni,i}nstall_lib_mysqludf_ta $INSTALLDIR/usr/share/libmysqludf_ta/
-fpm -t deb -d "mysql-server (>= 5.1)" --description "Trading technical analysis functions as MySQL UDFs" --url https://github.com/joaocosta/lib_mysqludf_ta/ -m "joaocosta@zonalivre.org" -s dir -n libmysqludf_ta -v 0.1 -C $INSTALLDIR usr
-fpm -t rpm -d "mysql-server >= 5.1" --description "Trading technical analysis functions as MySQL UDFs" --url https://github.com/joaocosta/lib_mysqludf_ta/ -m "joaocosta@zonalivre.org" -s dir -n libmysqludf_ta -v 0.1 -C $INSTALLDIR usr
+fpm -t deb -d "mysql-server (>= 5.1)" --description "Trading technical analysis functions as MySQL UDFs" --url https://github.com/joaocosta/lib_mysqludf_ta/ -m "joaocosta@zonalivre.org" -s dir -n libmysqludf_ta -v 0.1 --iteration $RELEASE -C $INSTALLDIR usr
+fpm -t rpm -d "mysql-server >= 5.1" --description "Trading technical analysis functions as MySQL UDFs" --url https://github.com/joaocosta/lib_mysqludf_ta/ -m "joaocosta@zonalivre.org" -s dir -n libmysqludf_ta -v 0.1 --iteration $RELEASE -C $INSTALLDIR usr
 
 rm -fR $INSTALLDIR
