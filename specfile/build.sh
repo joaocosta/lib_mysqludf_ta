c251743894981a350ed7f867ef088388e23004cf
João Costa <joaocosta@zonalivre.org>
2016-01-24 19:08:20 +0000

    Build in centos7 and sign

--- specfile/build.sh
+++ specfile/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-set -e
-cd ~/src
+set -euo pipefail
+
+cd ~/
 rm -fR ~/rpmbuild
 rpmdev-setuptree
 ln -vs lib_mysqludf_ta/ lib_mysqludf_ta-0.1
 tar --exclude=.git -hczPf ~/rpmbuild/SOURCES/lib_mysqludf_ta-0.1.tar.gz lib_mysqludf_ta-0.1/
 rm lib_mysqludf_ta-0.1
-rpmbuild -v -ba --clean lib_mysqludf_ta/specfile/lib_mysqludf_ta.spec
+rpmbuild -v -ba --clean --sign lib_mysqludf_ta/specfile/lib_mysqludf_ta.spec
