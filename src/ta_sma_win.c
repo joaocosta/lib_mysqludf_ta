4a0ddaaed00bcdca826b1debbe1bbfbb383db85d
João Costa <joaocosta@zonalivre.org>
2020-01-22 22:34:06 +0000

    Comment debug messages

--- src/ta_sma_win.c
+++ src/ta_sma_win.c
@@ -86,10 +86,10 @@ DLLEXP double ta_sma_win(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *
 {
 	ta_sma_win_data *data = (ta_sma_win_data *)initid->ptr;
 	int *periods = (int *)args->args[1];
-	double *value = (double*)args->args[0];
+	//double *value = (double*)args->args[0];
 
-    fprintf(stderr, "cal ta_sma_win %i %f arg_count=%i, current = %i\n", *periods, *value, args->arg_count, data->current);
-    fflush(stderr);
+    //fprintf(stderr, "cal ta_sma_win %i %f arg_count=%i, current = %i\n", *periods, *value, args->arg_count, data->current);
+    //fflush(stderr);
 	if (args->args[0] == NULL) {
 		if (data->current > 0) {
 			strcpy(error, "ta_sma_win() Can't handle NULL values in middle of dataset");
@@ -108,11 +108,13 @@ DLLEXP double ta_sma_win(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *
 		for (i = 0; i < *periods; i++)
 			sum += data->values[i];
 
+/*
         for (i = 0; i < *periods; i++) {
             fprintf(stderr, "cal ta_sma_win values[%i]=%f\n" , i, data->values[i]);
         }
         fprintf(stderr, "cal ta_sma_win sum=%f\n" , sum);
         fflush(stderr);
+*/
 
 		return sum / *periods;
 	}
@@ -125,26 +127,25 @@ void ta_sma_win_clear(UDF_INIT *initid, char *is_null, char *error) {
     data->current = data->current + 1;
     moveToNextSlot(data);
 
-    fprintf(stderr, "clear ta_sma_win, current=%i, next=%i\n", data->current, data->next_slot);
-    fflush(stderr);
+    //fprintf(stderr, "clear ta_sma_win, current=%i, next=%i\n", data->current, data->next_slot);
+    //fflush(stderr);
 }
 
 void ta_sma_win_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
 
     ta_sma_win_data *data = (ta_sma_win_data *)initid->ptr;
-    int *periods = (int *)args->args[1];
+    //int *periods = (int *)args->args[1];
     double *value = (double*)args->args[0];
 
     data->values[data->next_slot] = *value;
 
-    fprintf(stderr, "add ta_sma_win periods=%i value=%f, next_slot=%i\n", *periods, *value, data->next_slot);
-    fflush(stderr);
+    //fprintf(stderr, "add ta_sma_win periods=%i value=%f, next_slot=%i\n", *periods, *value, data->next_slot);
+    //fflush(stderr);
 
 }
 
 void ta_sma_win_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
 
-  fprintf(stderr, "reset ta_sma_win\n");
   int *periods = (int *)args->args[1];
   double *value = (double*)args->args[0];
 
